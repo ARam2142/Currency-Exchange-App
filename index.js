b1737a5edbbaaa08efdff7ce7aefb9d0a8d06831
Aram2142 <andresaaramirez@aol.com>
2021-04-22 23:33:34 -0400

    logging in keypress. not sure if i need this?

--- index.js
+++ index.js
@@ -9,40 +9,59 @@
 //submitting the convert button will give the user the equivalent currency amount to the selected currency
 
 //grab elements 
+const userInput = document.getElementById('userInput');
 const fromCurrency = document.getElementById("fromCurrency");
 const toCurrency = document.getElementById("toCurrency");
 const convertBtn = document.getElementById('convert');
 const result = document.querySelector('result');
 const apiKey = '7ebff14093ba09068127633d';
 
-function fetchApi(){
+function fetchApi() {
 
     fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`)
 
-    .then(response => { 
-        return response.json()
-               
-    }).then(data => {
-        //console.log(data)
-        let jsonData = data.conversion_rates;
-        const newArr = Object.keys(jsonData); console.log(newArr);
-        appendData(newArr)
-                
-    }).catch(err => {
-        console.log(err)
-    })
-    
+        .then(response => {
+            return response.json()
+
+        }).then(data => {
+            //console.log(data)
+            let jsonData = data.conversion_rates;
+            const newArr = Object.keys(jsonData); console.log(newArr);
+            appendData(newArr)
+            amount(newArr, jsonData)
+        }).catch(err => {
+            console.log(err)
+        })
+
     const appendData = (newArr) => {
-            console.log(newArr);
-            let output = ''
+        //console.log(newArr);
+        let output = ''
         for (let i = 0; i < newArr.length; i++) {
             output = `<option>${newArr[i]}</option>`;
             fromCurrency.innerHTML += output
             toCurrency.innerHTML += output
         }
-        
     }
-    
+
+    //get the value of what ever the user types in.
+    //get the value to match the currency from the api
+    //user chooses currency and then hits convert button which will display on the bottom
+
+    const amount = (newArr, jsonData) => {
+        //console.log(jsonData)
+        let monetaryVal = jsonData
+
+        for (const key in monetaryVal) {
+            console.log(`${key}: ${monetaryVal[key]}`)
+        }
+        userInput.addEventListener('keyup', e => {
+            let keyPress = e.target.value
+            console.log(keyPress)
+        });
+    }
+    amount()
+
+
 }
 
 fetchApi();
